2cdc7a8a2e5e48ca63e6b627dd027aef6648ce68
Christian Augustin <generalstrejk@gmail.com>
2022-02-07 20:43:33 +0100

    "less manual spaces"

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
-	{"", "echo \"   $(mailbox)\"",		10,									0},
+	{"", "mailbox",										10,									0},
 	{"", "localip",										10,									0},
 	{"", "curl ipinfo.io/ip",					10,									0},
 	{"", "trafdown",									1,									0},
