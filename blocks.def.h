87430aba35d0bd33f3444f4588dd9a5bf8a9d633
Christian Augustin <generalstrejk@gmail.com>
2022-02-07 21:44:28 +0100

    "clock and public ip as scripts"

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
 	{"", "mailbox",										10,									0},
 	{"", "localip",										10,									0},
-	{"", "curl ipinfo.io/ip",					10,									0},
+	{"", "publicip",									10,									0},
 	{"", "trafdown",									1,									0},
 	{"", "trafup",										1,									0},
 	{"", "cpu",												1,									0},
@@ -11,7 +11,7 @@ static const Block blocks[] = {
 	{"", "disk",											10,									0},
 	{"", "memory",										10,									0},
 	{"", "volume",										10,									0},
-	{"", "date '+%F %H:%M      '",		5,									0},
+	{"", "clock",											5,									0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
