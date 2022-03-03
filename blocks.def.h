c5206b21170470bd20605f5f2baa5bf87e9b2a65
Christian Augustin <generalstrejk@gmail.com>
2022-03-03 16:44:04 +0100

    "proper bar as delimiter"

--- blocks.def.h
+++ blocks.def.h
@@ -15,5 +15,5 @@ static const Block blocks[] = {
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " │ ";
 static unsigned int delimLen = 5;
