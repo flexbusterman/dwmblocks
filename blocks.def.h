7fc0adc2284388f3551ed75e061e540443d7a190
Christian Augustin <generalstrejk@gmail.com>
2022-04-03 19:00:13 +0200

    "delimeter"

--- blocks.def.h
+++ blocks.def.h
@@ -2,18 +2,29 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/							/*Update Interval*/	/*Update Signal*/
 	{"", "mailbox",										10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "localip",										10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "publicip",									10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "trafdown",									1,									0},
+	{"", "delimeter",									10,									0},
 	{"", "trafup",										1,									0},
+	{"", "delimeter",									10,									0},
 	{"", "cpu",												1,									0},
+	{"", "delimeter",									10,									0},
 	{"", "battery",										10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "disk",											10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "memory",										10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "volume",										10,									0},
+	{"", "delimeter",									10,									0},
 	{"", "clock",											5,									0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " │ ";
+// static char delim[] = " │ ";
+static char delim[] = "\0";
 static unsigned int delimLen = 5;
